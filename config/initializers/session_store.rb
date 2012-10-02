22b1034cd6232296d8fed6c557c3400b1e27fcdb
Ihor Breza <seniorigor@gmail.com>
2012-10-02 12:21:56 +0300

    start of the project

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RubyTestProject::Application.config.session_store :cookie_store, key: '_ruby-test-project_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RubyTestProject::Application.config.session_store :active_record_store
