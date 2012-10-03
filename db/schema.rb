0029d4ea8af47161b0784dddf92545eec6ba07f8
taras <taras@taras-M11x.(none)>
2012-10-03 18:15:25 +0300

    user_image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121002171250) do
+ActiveRecord::Schema.define(:version => 20121003104857) do
+
+  create_table "photos", :force => true do |t|
+    t.string   "image_path"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
