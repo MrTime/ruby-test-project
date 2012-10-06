cee69c933234c535dd2f5c15ba63b4a99700aaaf
Paul Ihnatolia <ihnatolia3@gmail.com>
2012-10-06 15:17:53 +0300

    Fixed db problem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121005105846) do
+ActiveRecord::Schema.define(:version => 20121006105648) do
 
   create_table "books", :force => true do |t|
     t.string   "author"
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(:version => 20121005105846) do
     t.decimal  "price",       :precision => 10, :scale => 0
     t.datetime "created_at",                                 :null => false
     t.datetime "updated_at",                                 :null => false
-    t.integer  "user_id"
     t.string   "owner_login"
+    t.integer  "user_id"
   end
 
   create_table "photos", :force => true do |t|
