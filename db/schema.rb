15de55ede3a59695c08a00f71867ebdcfc1afa79
MrTime <kfedia@gmail.com>
2012-10-06 13:02:50 +0300

    updated schema rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,16 @@
 
 ActiveRecord::Schema.define(:version => 20121003104857) do
 
+  create_table "books", :force => true do |t|
+    t.string   "author"
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price",       :precision => 10, :scale => 0
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
+    t.integer  "user_id"
+  end
+
   create_table "photos", :force => true do |t|
     t.string   "image_path"
     t.integer  "user_id"
@@ -38,12 +48,4 @@ ActiveRecord::Schema.define(:version => 20121003104857) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
-  create_table "books", :force => true do |t|
-    t.string   "author"
-    t.string   "title"
-    t.text     "description"
-    t.decimal  "price",       :precision => 10, :scale => 0
-    t.datetime "created_at",                                 :null => false
-    t.datetime "updated_at",                                 :null => false
-  end
 end
