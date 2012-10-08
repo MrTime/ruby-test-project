da1eebcacc7e404cda7701ebfe9bad6780833477
taras <taras.my.mail@gmail.com>
2012-10-08 14:50:49 +0300

    single_photo_controller

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,32 @@
+class PhotosController < ApplicationController
+
+  def new
+    @photo = Photo.new
+	  if params.has_key?("id")
+	    @book = Book.find(params[:id])
+	  end
+  end
+
+  def add
+  	@photo  = Photo.new
+    uploaded_io = params[:photo][:picture]
+    File.open(Rails.root.join('app','assets', 'images', uploaded_io.original_filename), 'wb+') do |file|
+      file.write(uploaded_io.read)
+    end
+
+    @photo.image_path  =  uploaded_io.original_filename
+
+    if params[:photo].has_key?("book_id")
+  	  @photo.book_id = params[:photo][:book_id]
+  	  @photo.save
+  	  redirect_to("/books/#{params[:photo][:book_id]}")
+  	else
+  	  @photo.user_id = current_user.id
+  	  @photo.save
+  	  if current_user.photo
+        current_user.photo = @photo
+      end
+      redirect_to("/users/user_page")
+  	end  	  
+  end
+end
