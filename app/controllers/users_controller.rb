d2d9a4324c51f3b4aa67a0c90ef7436c9e475094
taras <taras.my.mail@gmail.com>
2012-10-10 18:34:20 +0300

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,24 +10,4 @@ class UsersController < ApplicationController
 
   def successful_registration
   end
-
-  def new_photo
-    @photo = Photo.new
-  end
-
-  def add_photo
-    uploaded_io = params[:photo][:picture]
-    File.open(Rails.root.join('app','assets', 'images', uploaded_io.original_filename), 'wb+') do |file|
-      file.write(uploaded_io.read)
-    end
-    @photo  = Photo.new
-    @photo.user_id = current_user.id
-    @photo.save
-    @photo.image_path  =  uploaded_io.original_filename
-    @photo.save
-    if current_user.photo
-      current_user.photo = @photo
-    end
-    redirect_to("/users/user_page")
-  end
 end	
