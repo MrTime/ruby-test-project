91ff02dba6df077b9c4f5bb892e439403069b02a
Paul Ihnatolia <ihnatolia3@gmail.com>
2012-10-12 00:46:52 +0300

    Change to root_path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def after_inactive_sign_up_path_for(resource)
-    "/"
+   root_path
   end
+
   def edit
     @photo = Photo.new
   end
+
 end
