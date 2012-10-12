3fc7d14a8d3b2389d914d6f5038ef3a04308cb35
Paul Ihnatolia <ihnatolia3@gmail.com>
2012-10-12 10:58:21 +0300

    Fix image uploading

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,7 +8,8 @@ class PhotosController < ApplicationController
   end
 
   def create
-=begin
+    #puts params.inspect
+
   	@photo  = Photo.new
     uploaded_io = params[:photo][:picture]
     File.open(Rails.root.join('app','assets', 'images', uploaded_io.original_filename), 'wb+') do |file|
@@ -17,22 +18,21 @@ class PhotosController < ApplicationController
 
     @photo.image_path  =  uploaded_io.original_filename
 
-    if params[:photo].has_key?("book_id")
-      @book = Book.find(params[:photo][:book_id])
+    if params.has_key?("book_id")
+      @book = Book.find(params[:book_id])
   	  @photo.book_id = @book.id
   	  @photo.save
   	  if @book.photo
         @book.photo = @photo
       end
-  	  redirect_to("/books/#{params[:photo][:book_id]}")
-  	else
+  	  redirect_to("/books/#{params[:book_id]}")
+    elsif params.has_key?("user_id")
   	  @photo.user_id = current_user.id
   	  @photo.save
   	  if current_user.photo
         current_user.photo = @photo
       end
-      redirect_to("/users/user_page")
+      redirect_to("/users/#{params[:user_id]}")
   	end
-=end
   end
 end
