27a3d438709cdf83cc6e1ce9a517c1a753667219
fizzik <fizzik@ubuntu.ubuntu-domain>
2012-10-12 13:28:27 +0300

    1)As user I want to see comments for selected book with content, author and time of creating,2) As user I want to add comments for book,3) As user I want to see header & footer like on twitter

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,6 @@ class Book < ActiveRecord::Base
   attr_accessible :author, :description, :price, :title, :user_id
 
   has_one :photo
+  has_many :comments
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121006105648) do
+ActiveRecord::Schema.define(:version => 20121011200019) do
 
   create_table "books", :force => true do |t|
     t.string   "author"
     t.string   "title"
     t.text     "description"
-    t.decimal  "price",       :precision => 10, :scale => 0
+    t.decimal  "price",       :precision => 10, :scale => 2
     t.datetime "created_at",                                 :null => false
     t.datetime "updated_at",                                 :null => false
-    t.integer  "user_id"
     t.string   "owner_login"
+    t.integer  "user_id"
+  end
+
+  create_table "comments", :force => true do |t|
+    t.text     "content"
+    t.integer  "book_id"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "photos", :force => true do |t|
