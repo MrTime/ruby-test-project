6ec086a651759da87247d410e534266c9800895a
Popovich-Jurij <popovich.jurij@yandex.ru>
2012-10-15 18:35:49 +0300

    Full search using regular expressions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,22 @@
 class SearchController < ApplicationController
 
   def search_books
-    search_input = "%#{params[:search_title]}%"
+    search_input = "#{params[:search_title]}"
+    search_input = search_input.match(/[^?*]*/).to_s
 
-    search = search_input.gsub!(/'[^']*'/).to_a
-    search.collect{|n| n.gsub!("'", "") }
-    search << search_input.split(" ")
-    search.flatten!
-    search = search.join("|")
+    if search_input == ""
+      "Book whith title or author or description or price '#{params[:search_title]}' not found  !!!"
+    else
+      search = search_input.gsub!(/'[^']*'/).to_a
+      search.collect{|n| n.gsub!("'", "") }
+      search << search_input.split(" ")
+      search.flatten!
+      search = search.join("|")
+    end
 
-    @book=Book.where('title LIKE ? or author LIKE ? or description LIKE ? or price LIKE ? or title RLIKE ? or author RLIKE ? or description RLIKE ? or price RLIKE ?', search, search, search, search, search, search, search, search  ).all
+
+
+    @book=Book.where('title RLIKE ? or author RLIKE ? or description RLIKE ? or price RLIKE ?', search, search, search, search).all
 
   end
 end
