faf162c9d1329f4756e0d891f70442aa7b0fdc99
Vitaleks2012 <vitaleks2012@i.ua>
2012-10-19 03:09:57 +0400

    As user I want to have nice design for book comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     @comment.book_id = params[:book_id].to_i
     if @comment.save
       respond_to do |format|
-        format.js {render :js => "jQuery('.comments tr:first').before('<tr><td>#{current_user.email}</td><td>#{@comment.created_at}</td><td>#{@comment.content}</td><td><button>X</button></td></tr>');jQuery('#content').val('');"}
+        format.js {render :js => "jQuery('.comments tr:first').before('<tr><td>#{current_user.email}</td><td>#{@comment.created_at}</td><td>#{@comment.content}</td><td><button>Delete</button></td></tr>');jQuery('#content').val('');"}
         format.html { redirect_to :back}
       end
     end
