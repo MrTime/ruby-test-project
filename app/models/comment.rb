f5138fe94042ffef8a07daf7c9eceb95d5eee16b
Vitaleks2012 <vitaleks2012@i.ua>
2012-10-27 17:06:35 +0400

    As user I want to see correct sign in page design with no functional bugs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   attr_accessible :content, :user_id, :book_id
   belongs_to :book
   belongs_to :user
+
+  validates_presence_of :content
+
+  scope :desc, order("created_at DESC")
 end
